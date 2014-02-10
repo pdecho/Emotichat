02f72cb577ae73fdd548dafe499872eada7426ae
Blue is the warmest color <Decho@Blue-is-the-warmest-color.local>
2014-02-09 23:39:31 -0500

    rename folder + delete unused examples

---
+++ prototype/emotichat prototype 1/src/testApp.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxFaceTracker.h"
+#include "Word.h"
+
+class testApp : public ofBaseApp {
+public:
+    
+    
+    
+	void setup();
+	void update();
+	void draw();
+	void keyPressed(int key);
+    void keyReleased(int key);
+
+
+    string userInput;
+	
+	ofVideoGrabber cam;
+	ofxFaceTracker tracker;
+	ExpressionClassifier classifier;
+    
+    ofTrueTypeFont normalFont;
+    ofTrueTypeFont angryFont;
+    ofTrueTypeFont happyFont;
+    
+    ofColor normalColor;
+    ofColor happyColor;
+    ofColor angryColor;
+    
+    
+    float normalValue=0;
+    float angryValue=0;
+    float happyValue=0;
+    
+    vector<ofTrueTypeFont> fonts;
+    
+    Word input; // <-- this is the word that the user is typing at any given time
+    vector<Word> words; // <-- this is the list of words that the user has typed in the past.
+    
+    int 				snapCounter;
+    bool 				bSnapshot;
+    ofImage             img;
+    char 				snapString[255];
+
+};
